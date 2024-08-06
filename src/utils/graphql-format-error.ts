0de67e266a51360093529cf40358758482be228a
Mubashir Hassan <mhm13devk@gmail.com>
2024-08-06 14:48:22 +0500

    feat: improve graphqlFormatError

--- src/utils/graphql-format-error.ts
+++ src/utils/graphql-format-error.ts
@@ -12,30 +12,31 @@ export const graphqlFormatError = (
   // unwrapResolverError removes the outer GraphQLError wrapping from
   // errors thrown in resolvers, enabling us to check the instance of
   // the original error
-  const unwrappedError = unwrapResolverError(error) as HttpException;
+  const unwrappedError = unwrapResolverError(error);
 
   if (unwrappedError instanceof HttpException) {
+    const httpException = unwrappedError as HttpException;
     return {
       ...formattedError,
       extensions: formattedError.extensions
         ? {
             ...formattedError.extensions,
-            code: unwrappedError.name,
+            code: httpException.name,
           }
         : undefined,
     };
   }
 
-  const internalServerError = new InternalServerErrorException(
+  const internalServerErrorException = new InternalServerErrorException(
     'Something went wrong',
   );
   return {
     ...formattedError,
-    message: internalServerError.message,
+    message: internalServerErrorException.message,
     extensions: {
-      code: internalServerError.name,
-      status: internalServerError.getStatus(),
-      originalError: internalServerError.getResponse(),
+      code: internalServerErrorException.name,
+      status: internalServerErrorException.getStatus(),
+      originalError: internalServerErrorException.getResponse(),
     },
   };
 };
